8658c9cef04c1aeaa5acc73a776f40c738c6a857
saurabh2423 <mahajansaurabh2423@gmail.com>
2023-11-01 00:54:18 +0530

    first commit

---
+++ SRC/day1.py
@@ -0,0 +1,57 @@
+
+print('hello')
+
+# 1.
+x = 10
+print(x)
+print(type(x))
+
+# 2.
+q = 20.9
+print(q)
+print(type(q))
+
+# 3.
+w = True
+print(w)
+print(type(w))
+
+# 4.
+e = 'saurabh'
+print(e)
+print(type(e))
+
+
+# collections [list,tuple,dictionary,set]
+
+# list
+
+q1 = ['pune','delhi','mumbai','banglore']
+q2 = [11,22,33,44]
+print(q1)
+print(q2)
+print(type(q1))
+print(type(q2))
+
+# tuple (fixed length)
+q3 = ("ram","sham","satish","sachin")
+print(q3)
+print(type(q3))
+
+
+# set (does not allow duplicate values)
+
+q4 = {"chinmay","sarika","poorva","ram","chinmay"}
+print(q4)
+print(type(q4))
+
+
+# dictionary
+q5 = {
+    'firstname' : 'saurabh',
+    'lastname' : 'mahajan',
+    "age": 29,
+    "rollNo": 88
+}
+print(q5)
+print(type(q5))
---
+++ SRC/day2.py
@@ -0,0 +1,44 @@
+
+
+# operators
+
+# 1 arithmatic
+a = 10
+b = 20
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+
+c = 'saurabh'
+d = 'mahajan'
+print(c + d)
+
+
+
+# 2 comparisson operator
+# and / or / not
+
+print(12 > 4)
+print(12 < 4)
+print(12 == 4)
+print(12 != 4)
+print(12 == 12)
+print(12 >= 4)
+print(12 <= 4)
+print(12 >= 13)
+
+# and
+
+print(3 == 3 and 4 == 4)
+print(3 != 3 and 4 == 4)
+
+
+# or
+print(3 == 3 or 4 == 4)
+print(3 != 3 or 4 == 4)
+
+# not
+print(not(3 == 3))
+print(not(3 != 3))
