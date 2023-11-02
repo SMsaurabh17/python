ace9082752a934df74498eb8216bd918e5beea80
saurabh2423 <mahajansaurabh2423@gmail.com>
2023-11-02 18:51:14 +0530

    added

--- SRC/day4.py
+++ SRC/day4.py
@@ -1,1 +1,36 @@
-print(10101)
+
+
+# for loop
+
+# 1
+for i in range(10):
+    print(i)
+
+# 2
+for i in range(6):
+    print((i))
+
+
+# 3
+for x in range (1,9):
+    print(x)
+
+
+# 4
+for z in range (3):
+    print('hello')
+
+# 5
+for c in range (2,21,2):
+    print(c)
+
+
+# 6
+for d in range(20,0,-2):
+    print(d)
+
+# 7 using break statement
+for e in range (1,9):
+    if e == 5:
+        break
+    print(e)
